ce35df11e3ae68c07d3142f1a21fc778117b0d2e
Rodrigo Ramalho Araújo <rodrigoraraujo@gmail.com>
2017-10-23 15:49:50 -0200

    Removed db.json from repository

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,8 +7,20 @@ export function getUsers() {
     return get('users');
 }
 
+export function deleteUser(id) {
+    return del(`users/${id}`);
+}
+
 function get(url) {
     return fetch(baseURL + url).then(onSuccess, onError);
+}
+
+function del(url) {
+    const request = new Request(baseURL + url, {
+        method: "DELETE"
+    });
+
+    return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 getUsers().then(result => {
     let userBody = "";
@@ -13,4 +13,16 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = userBody;
+
+    const deleteLinks = global.document.getElementsByClassName("deleteUser");
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        }
+    });
 });
