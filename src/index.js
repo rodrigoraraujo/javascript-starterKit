bd2d8f97f298f08368426283ea91f0f4e4713e93
Rodrigo R. Araújo <rodrigoraraujo@gmail.com>
2017-10-28 12:25:27 -0200

    Production build, bundling css, logging

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import { getUsers, deleteUser } from './api/userApi';
 
 getUsers().then(result => {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,56 +2,59 @@ import path from "path";
 import webpack from "webpack";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import WebpackMd5Hash from "webpack-md5-hash";
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-    debug: true,
-    devtool: "source-map",
-    noInfo: false,
-    entry: {
-        vendor: path.resolve(__dirname, "src/vendor"),
-        main: path.resolve(__dirname, "src/index")
-    },
-    target: "web",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
-        filename: "[name].[chunkhash].js"
-    },
-    plugins: [
-        new WebpackMd5Hash(),
+  debug: true,
+  devtool: "source-map",
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, "src/vendor"),
+    main: path.resolve(__dirname, "src/index")
+  },
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[chunkhash].js"
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new WebpackMd5Hash(),
 
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        }),
-        //Create HTML file that includes reference to bundled JS
-        new HtmlWebPackPlugin({
-            template: 'src/index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyCSS: true,
-                minifyJS: true,
-                minifyURLs: true
-            }
-        }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Create HTML file that includes reference to bundled JS
+    new HtmlWebPackPlugin({
+      template: 'src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true
+      },
+      trackJSToken: '43ad216f57d94259968435894490a5c7'
+    }),
 
-        //Eliminate duplicate packages when generating bundle
-        new webpack.optimize.DedupePlugin(),
+    //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
 
-        //Minify JS
-        new webpack.optimize.UglifyJsPlugin()
-    ],
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
-            { test: /\.css$/, loaders: ["style", "css"] }
-        ]
-    }
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
 
-}
+};
